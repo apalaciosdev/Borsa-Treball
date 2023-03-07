ccce2ffaced8c28d0d4214cc6ffb6d98262cc20c
Aaron Palacios <apalacios.dev@gmail.com>
2023-03-07 19:07:21 +0100

    remove funcion inecesaria

--- BorsaTreballApp/app/Http/Controllers/UsuariosController.php
+++ BorsaTreballApp/app/Http/Controllers/UsuariosController.php
@@ -7,15 +7,6 @@ use Illuminate\Http\Request;
 
 class UsuariosController extends Controller
 {
-  public function login()
-  {
-    if (true) {  //TODO: Si el usuario no está registrado, entonces retornar la vista de login
-      return view('auth.usuarios.login');
-    } else { //TODO: Si lo está, retornar la vista de la página inicial de usuarios
-      return view('auth.usuarios.login');
-    }
-  }
-
   public function register()
   {
     return view('usuarios.register');
