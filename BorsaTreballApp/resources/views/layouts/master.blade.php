cb3813a0f846698f4fdc6b557d34d638bb336117
bclara2001 <115286129+bclara2001@users.noreply.github.com>
2023-03-09 16:12:11 +0100

    Update logo

--- BorsaTreballApp/resources/views/layouts/master.blade.php
+++ BorsaTreballApp/resources/views/layouts/master.blade.php
@@ -5,12 +5,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Borsa de Treball</title>
+    <link rel="shortcut icon" href="/logo.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
 <body>
-    @yield('content')
+    @yield('content')   
 </body>
 <script src="https://code.jquery.com/jquery-3.6.3.min.js"
     integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
