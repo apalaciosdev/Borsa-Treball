93d8b976ddcf471cab9ed95ede647c24fb90efe1
Aaron Palacios <apalacios.dev@gmail.com>
2023-03-14 19:18:40 +0100

    update modelo usuario

--- BorsaTreballApp/app/Models/Usuario.php
+++ BorsaTreballApp/app/Models/Usuario.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Usuario extends Model
 {
-    protected $fillable = ['nombre', 'apellidos', 'descripcion', 'email', 'password', 'fechaNacimiento', 'titulacion', 'cochePropio', 'experienciaLaboral'];
+    protected $fillable = ['id', 'nombre', 'apellidos', 'descripcion', 'email', 'password', 'fechaNacimiento', 'titulacion', 'cochePropio', 'experienciaLaboral'];
     public $timestamps = false;
     protected $primaryKey = 'email';
     public $incrementing = false;
