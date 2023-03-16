270f46459f6c76ade3b870eefabf7e5b7f7c94c4
bclara2001 <115286129+bclara2001@users.noreply.github.com>
2023-03-16 17:10:52 +0100

    Estetica registro usuario

--- BorsaTreballApp/resources/views/usuarios/register.blade.php
+++ BorsaTreballApp/resources/views/usuarios/register.blade.php
@@ -1,70 +1,62 @@
 @extends('layouts.master')
 
 @section('content')
-  
-<div class="row" style="margin-top:40px">
-   <div class="offset-md-3 col-md-6">
-      <div class="card">
-         <div class="card-body">
-            <p class="text-center">Registro</p>
-            
-            <form method="POST" action="{{ url('/user/añadirUsuario') }}">
-               @csrf
-               <div class="form-group">
-                  <label for="nombre">Nombre</label>
-                  <input type="text" name="nombre" id="nombre" class="form-control" required>
-               </div>
-   
-               <div class="form-group">
-                  <label for="apellidos">Apellidos</label>
-                  <input type="text" name="apellidos" id="apellidos" class="form-control" required>
-               </div>
-    
-               <div class="form-group">
-                   <label for="email">Email</label>
-                   <input type="text" name="email" id="email" class="form-control" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required>
+    <div class="container">
+        <div class="row my-5">
+            <div class="offset-md-3 col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title text-center">Registro</h4>
+                        <form method="POST" action="{{ url('/user/añadirUsuario') }}" class="row">
+                            @csrf
+                            <div class="col-5 mb-2">
+                                <label for="nombre">Nombre</label>
+                                <input type="text" name="nombre" id="nombre" class="form-control" required>
+                            </div>
+                            <div class="col-7 mb-2">
+                                <label for="apellidos">Apellidos</label>
+                                <input type="text" name="apellidos" id="apellidos" class="form-control" required>
+                            </div>
+                            <div class="col-12 mb-2">
+                                <label for="email">Email</label>
+                                <input type="text" name="email" id="email" class="form-control"
+                                    pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required>
+                            </div>
+                            <div class="col-8 mb-2">
+                                <label for="password">Password </label>
+                                <input type="password" name="password" id="password" class="form-control" required>
+                            </div>
+                            <div class="col-4 mb-2">
+                                <label for="fechaNacimiento">Fecha de nacimiento</label>
+                                <input type="date" name="fechaNacimiento" id="fechaNacimiento" class="form-control"
+                                    required>
+                            </div>
+                            <div class="col-12 mb-2">
+                                <label for="descripcion">Descripción</label>
+                                <textarea name="descripcion" id="descripcion" class="form-control" rows="3" required></textarea>
+                            </div>
+                            <div class="col-9 mb-2">
+                                <label for="titulacion">Titulación </label>
+                                <input type="text" name="titulacion" id="titulacion" class="form-control" required>
+                            </div>
+                            <div class="col-3 mb-2">
+                                <label for="cochePropio">Coche propio </label>
+                                <select name="cochePropio" id="cochePropio" class="form-control">
+                                    <option value="1">SI</option>
+                                    <option value="0">NO</option>
+                                </select>
+                            </div>
+                            <div class="col-12 mb-2">
+                                <label for="experienciaLaboral">Experiencia Laboral</label>
+                                <textarea name="experienciaLaboral" id="experienciaLaboral" class="form-control" rows="3" required></textarea>
+                            </div>
+                            <div class="col-12 text-center">
+                                <button type="submit" class="btn btn-primary w-50">Registrarse</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-    
-                <div class="form-group">
-                   <label for="descripcion">Descripción</label>
-                   <textarea name="descripcion" id="descripcion" class="form-control" rows="3" required></textarea>
-                </div>
-   
-                <div class="form-group">
-                   <label for="fechaNacimiento">Fecha de nacimiento</label>
-                   <input type="date" name="fechaNacimiento" id="fechaNacimiento" class="form-control" required>
-                </div>
-   
-                <div class="form-group">
-                   <label for="titulacion">Titulación </label>
-                   <input type="text" name="titulacion" id="titulacion" class="form-control" required>
-                </div>
-    
-                <div class="form-group">
-                  <label for="cochePropio">Coche propio </label>
-                   <select name="cochePropio" id="cochePropio" class="form-control">
-                       <option value="1">SI</option>
-                       <option value="0">NO</option>
-                   </select>
-                </div>
-                
-                <div class="form-group">
-                   <label for="experienciaLaboral">Experiencia Laboral</label>
-                   <textarea name="experienciaLaboral" id="experienciaLaboral" class="form-control" rows="3" required></textarea>
-                </div>
-                <div class="form-group">
-                   <label for="password">Password </label>
-                   <input type="password" name="password" id="password" class="form-control" required>
-                </div>
-                <div class="form-group text-center">
-                  <button type="submit" class="btn btn-primary" style="padding:8px 100px;margin-top:25px;">
-                     Registrarse
-                  </button>
-                </div>
-            </form>
-         </div>
-      </div>
-   </div>
- </div>
-
+            </div>
+        </div>
+    </div>
 @stop
