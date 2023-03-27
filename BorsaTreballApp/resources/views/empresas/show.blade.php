bbc27b12d8bb9c81aa2c1665a896e3effebf28bd
bclara2001 <115286129+bclara2001@users.noreply.github.com>
2023-03-27 18:44:36 +0200

    Update show.blade.php

--- BorsaTreballApp/resources/views/empresas/show.blade.php
+++ BorsaTreballApp/resources/views/empresas/show.blade.php
@@ -8,36 +8,36 @@
                 <div class="card-body">
                     <form class="row" method="POST">
                         @csrf
-                        <input type="hidden" name="lastEmail" value='{{ $empresa->email }}'>
+                        <input type="hidden" name="lastEmail" value="{{ $empresa->email }}"">
                         <div class="col-3 mb-3">
                             <label for="nombre" class="form-label">Nombre de la empresa</label>
                             <input type="text" class="form-control" id="nombre" name="nombre"
-                                value='{{ $empresa->nombre }}' disabled>
+                                value="{{ $empresa->nombre }}"" disabled>
                         </div>
                         <div class="col-6 mb-3">
                             <label for="direccion" class="form-label">Dirección de la empresa</label>
                             <input type="text" class="form-control" id="direccion" name="direccion"
-                                value='{{ $empresa->direccion }}' disabled>
+                                value="{{ $empresa->direccion }}" disabled>
                         </div>
                         <div class="col-3 mb-3">
                             <label for="localidad" class="form-label">Localidad</label>
                             <input type="text" class="form-control" id="localidad" name="localidad"
-                                value='{{ $empresa->localidad }}' disabled>
+                                value="{{ $empresa->localidad }}" disabled>
                         </div>
                         <div class="col-2 mb-3">
                             <label for="puesto" class="form-label">Puesto ofertado</label>
                             <input type="text" class="form-control" id="puesto" name="puesto"
-                                value='{{ $oferta->nombrePuesto }}' readonly>
+                                value="{{ $oferta->nombrePuesto }}" readonly>
                         </div>
                         <div class="col-3 mb-3">
                             <label for="salario" class="form-label">Salario</label>
                             <input type="number" class="form-control" id="salario" name="salario"
-                            value='{{ $oferta->salario }}' readonly>
+                            value="{{ $oferta->salario }}" readonly>
                         </div>
                         <div class="col-3 mb-3">
                             <label for="data" class="form-label">Data de publicación</label>
                             <input type="date" class="form-control" id="data" name="data"
-                                value='{{ $oferta->fechaPublicacion }}' readonly>
+                                value="{{ $oferta->fechaPublicacion }}" readonly>
                         </div>
                         <div class="col-4 mb-3">
                             <label for="estado" class="form-label">Estado</label>
@@ -49,9 +49,9 @@
                         </div>
                         <div class="col-12 mb-3">
                             <label for="descripcion" class="form-label">Descripción</label>
-                            <textarea readonly>{{ $ofera->descripcion }}</textarea>
+                            <textarea id="descripcion" class="form-control" readonly>{{ $oferta->descripcion }}</textarea>
                         </div>
-                        <div class="col-2 botones">
+                        <div class="col-2 offset-10 botones">
                             <button type="button" id="editarDatosOferta" class="btn btn-warning w-100">Editar oferta</button>
                             <button type="submit" class="d-none btn btn-primary w-100">Guardar oferta</button>
                         </div>
