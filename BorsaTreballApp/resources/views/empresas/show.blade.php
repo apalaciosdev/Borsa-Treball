8c97eb633fccdc97ab440c685cbe4c13212cda7b
bclara2001 <115286129+bclara2001@users.noreply.github.com>
2023-03-27 17:45:23 +0200

    Update show.blade.php

--- BorsaTreballApp/resources/views/empresas/show.blade.php
+++ BorsaTreballApp/resources/views/empresas/show.blade.php
@@ -1,58 +1,67 @@
 @extends('layouts.master')
 
 @section('content')
-
     @include('partials.navbar')
     <div class="container">
         <div class="row mt-3">
             <div class="col-12 card">
                 <div class="card-body">
-                    <form class="row">
+                    <form class="row" method="POST">
+                        @csrf
+                        <input type="hidden" name="lastEmail" value='{{ $empresa->email }}'>
                         <div class="col-3 mb-3">
                             <label for="nombre" class="form-label">Nombre de la empresa</label>
                             <input type="text" class="form-control" id="nombre" name="nombre"
-                                value={{ $oferta->nombre }} readonly>
+                                value='{{ $empresa->nombre }}' disabled>
                         </div>
-                        <div class="col-7 mb-3">
+                        <div class="col-5 mb-3">
                             <label for="direccion" class="form-label">Dirección de la empresa</label>
                             <input type="text" class="form-control" id="direccion" name="direccion"
-                                value={{ $oferta->direccion }} readonly>
+                                value='{{ $empresa->direccion }}' disabled>
                         </div>
                         <div class="col-2 mb-3">
                             <label for="localidad" class="form-label">Localidad</label>
                             <input type="text" class="form-control" id="localidad" name="localidad"
-                                value={{ $oferta->localidad }} readonly>
+                                value='{{ $empresa->localidad }}' disabled>
                         </div>
-
-                        <div class="col-12 form-group mb-3">
-                            <label for="nomPue" class="form-label">Nombre del puesto: </label>
-                            <input type="text" name="nomPue" id="nomPue" class="form-control"
-                                value={{ $oferta->nombrePuesto }} readonly>
+                        <div class="col-2 mb-3">
+                            <label for="puesto" class="form-label">Puesto ofertado</label>
+                            <input type="text" class="form-control" id="puesto" name="puesto"
+                                value='{{ $oferta->nombrePuesto }}' readonly>
                         </div>
-
-                        <div class="col-12 form-group mb-3">
-                            <label for="descr">Descripción: </label>
-                            <textarea name="descr" id="descr" class="form-control" value={{ $oferta->descripcion }} readonly></textarea>
+                        <div class="col-2 mb-3">
+                            <label for="descripcion" class="form-label">Descripción</label>
+                            <textarea readonly>{{ $ofera->descripcion }}</textarea>
                         </div>
-
-                        <div class="col-12 form-group mb-3">
-                            <label for="localidad">Localidad: </label>
-                            <input type="text" name="localidad" id="localidad" class="form-control" value={{ $oferta->localidadOferta }} readonly>
+                        <div class="col-4 mb-3">
+                            <label for="salario" class="form-label">Salario</label>
+                            <input type="number" class="form-control" id="salario" name="salario"
+                                value='{{ $oferta->salario }}' readonly>
                         </div>
-
-                        <div class="col-12 form-group mb-3">
-                            <label for="salario">Salario: </label>
-                            <input type="float" name="salario" id="salario" class="form-control" value={{ $oferta->salario }} readonly>
+                        <div class="col-4 mb-3">
+                            <label for="data" class="form-label">Data de publicación</label>
+                            <input type="date" class="form-control" id="data" name="data"
+                                value='{{ $empresa->email }}' readonly>
                         </div>
-
-                        <div class="col-2 offset-10">
-                            <button type="button" class="btn btn-warning w-100">Editar datos</button>
-                            <button type="button" class="d-none btn btn-primary w-100">Guardar datos</button>
+                        <div class="col-4 mb-3">
+                            <label for="estado" class="form-label">Estado</label>
+                            <select id="estado" name="estado">
+                                <option>Vigente</option>
+                                <option>Caducada</option>
+                                <option>Cubierta</option>
+                            </select>
+                        </div>
+                        <div class="col-2 botones">
+                            <button type="button" id="editarOferta" class="btn btn-warning w-100">Editar oferta</button>
+                            <button type="submit" class="d-none btn btn-primary w-100">Guardar oferta</button>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
-
 @stop
+
+{{-- @section('scripts')
+    <script type="text/javascript" src={{ asset('js/empresas.js') }}></script>
+@stop --}}
