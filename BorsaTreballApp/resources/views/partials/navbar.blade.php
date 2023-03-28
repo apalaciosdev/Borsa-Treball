b6ca09acf738eb9a2d03fe5b69a20dad5a0c10e7
Aaron Palacios <apalacios.dev@gmail.com>
2023-03-28 20:25:05 +0200

    update navbar

--- BorsaTreballApp/resources/views/partials/navbar.blade.php
+++ BorsaTreballApp/resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="/" style="color:#777"><span style="font-size:15pt">&#9820;</span> Borsa de Treball</a>
+        <a class="navbar-brand" href="{{session('rol')=='usuario' ? url('/user') : url('/empresas')}}" style="color:#777"><span style="font-size:15pt">&#9820;</span> Borsa de Treball</a>
 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -8,14 +8,6 @@
 
         @if( true || Auth::check() )
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{session('rol')=='usuario' ? url('/user') : url('/empresas')}}">
-                            <span class="glyphicon glyphicon-film" aria-hidden="true"></span>
-                            Home
-                        </a>
-                    </li>
-                </ul>
                 
                 @if (session('rol') == 'empresa')
                     <ul class="navbar-nav navbar-right">
